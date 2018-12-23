4e6da7324668ccd2f1362a5cfb95a3e0c0000ef3
huangdawei <david@hdw.im>
2018-12-23 11:29:01 +0800

    custom some configs

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -138,7 +138,7 @@ checkBrowsers(paths.appPath, isInteractive)
 
 // Create the production build and print the deployment instructions.
 function build(previousFileSizes) {
-  console.log('Creating an optimized production build...');
+  console.log('Creating a production build...');
 
   let compiler = webpack(config);
   return new Promise((resolve, reject) => {
